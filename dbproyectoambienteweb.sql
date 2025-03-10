e23bfebd1528c148aea8b451b4c4d120f5ca0ccd
Royvin <royvin1205@gmail.com>
2025-03-09 20:55:02 -0600

    base de datos con create

--- dbproyectoambienteweb.sql
+++ dbproyectoambienteweb.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `dbproyectoambienteweb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `dbproyectoambienteweb`;
 -- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: dbproyectoambienteweb
@@ -95,6 +97,10 @@ LOCK TABLES `tbl_proveedores` WRITE;
 INSERT INTO `tbl_proveedores` VALUES (1,'Repuestos Rápidos S.A.','contacto@repuestosrapidos.com'),(2,'AutoPartes Express','ventas@autopartesexpress.com'),(3,'Mecánica Global','info@mecanicaglobal.com');
 /*!40000 ALTER TABLE `tbl_proveedores` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping events for database 'dbproyectoambienteweb'
+--
 
 --
 -- Dumping routines for database 'dbproyectoambienteweb'
@@ -285,4 +291,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-03-09 20:21:20
+-- Dump completed on 2025-03-09 20:54:40
